70cbe9bfa686a7310738887d608ba4f0879cb598
Luanne <viluan922@gmail.com>
2022-03-10 19:22:13 +0700

    commit trên git changes

--- demoLoginForm/demoLoginForm/Form1.cs
+++ demoLoginForm/demoLoginForm/Form1.cs
@@ -16,5 +16,10 @@ namespace demoLoginForm
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
